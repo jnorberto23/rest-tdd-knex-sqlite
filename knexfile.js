9ac4262a465157e721509f728a64ec74fe0097c8
jnorberto23 <joaovictor.norberto@outlook.com>
2021-10-06 17:04:25 -0300

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,14 @@ const __dirname = path.resolve();
 
 export default  {
     client: 'sqlite3',
+
     connection: {
-        filename: path.resolve(__dirname, 'src','database', 'database.sqlite' ),
+        filename: path.resolve(__dirname, 'src','database', 'database.sqlite'),
     },
+
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
+
     useNullAsDefault: true,
 };
